9dcb7fc5bf7e3e8cd86926d847d850bc1ce3a634
H. Vetinari <h.vetinari@gmx.com>
2022-07-21 21:30:51 +0200

    move windows to CMake builds

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,41 +1,28 @@
+@echo on
 
-:: Hack for winres.h being called winresrc.h on VS2008
-if %VS_MAJOR% LEQ 9 copy %RECIPE_DIR%\winres.h .
-if errorlevel 1 exit 1
+mkdir build
+cd build
 
-:: Build!
-nmake /f Makefile.vc CFG=release-dynamic RTLIBCFG=dynamic OBJDIR=output all
-if errorlevel 1 exit 1
+cmake -G Ninja ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DBUILD_SHARED_LIBS:BOOL=ON ^
+    -DWEBP_BUILD_CWEBP:BOOL=OFF ^
+    -DWEBP_BUILD_DWEBP:BOOL=OFF ^
+    -DWEBP_BUILD_EXTRAS:BOOL=OFF ^
+    -DWEBP_BUILD_GIF2WEBP:BOOL=OFF ^
+    -DWEBP_BUILD_IMG2WEBP:BOOL=OFF ^
+    -DWEBP_BUILD_LIBWEBPMUX:BOOL=ON ^
+    -DWEBP_BUILD_VWEBP:BOOL=OFF ^
+    -DWEBP_BUILD_WEBP_JS:BOOL=OFF ^
+    -DWEBP_BUILD_WEBPINFO:BOOL=OFF ^
+    -DWEBP_BUILD_WEBPMUX:BOOL=OFF ^
+    -DWEBP_LINK_STATIC:BOOL=OFF ^
+    ..
+if %ERRORLEVEL% neq 0 exit 1
 
-copy output\release-dynamic\%ARCH%\bin\libwebp.dll %LIBRARY_PREFIX%\bin\libwebp.dll
-if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\bin\libwebpdecoder.dll %LIBRARY_PREFIX%\bin\libwebpdecoder.dll
-if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\bin\libwebpdemux.dll %LIBRARY_PREFIX%\bin\libwebpdemux.dll
-if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\bin\libwebpmux.dll %LIBRARY_PREFIX%\bin\libwebpmux.dll
-if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\lib\libwebp_dll.lib %LIBRARY_PREFIX%\lib\libwebp.lib
-if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\lib\libwebpdecoder_dll.lib %LIBRARY_PREFIX%\lib\libwebpdecoder.lib
-if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\lib\libwebpdemux_dll.lib %LIBRARY_PREFIX%\lib\libwebpdemux.lib
-if errorlevel 1 exit 1
-copy output\release-dynamic\%ARCH%\lib\libwebpmux_dll.lib %LIBRARY_PREFIX%\lib\libwebpmux.lib
-if errorlevel 1 exit 1
+cmake --build .
+if %ERRORLEVEL% neq 0 exit 1
 
-:: Copy header files
-mkdir %LIBRARY_PREFIX%\include\webp\
-if errorlevel 1 exit 1
-copy src\webp\decode.h %LIBRARY_PREFIX%\include\webp\
-if errorlevel 1 exit 1
-copy src\webp\encode.h %LIBRARY_PREFIX%\include\webp\
-if errorlevel 1 exit 1
-copy src\webp\types.h %LIBRARY_PREFIX%\include\webp\
-if errorlevel 1 exit 1
-copy src\webp\mux.h %LIBRARY_PREFIX%\include\webp\
-if errorlevel 1 exit 1
-copy src\webp\mux_types.h %LIBRARY_PREFIX%\include\webp\
-if errorlevel 1 exit 1
-copy src\webp\demux.h %LIBRARY_PREFIX%\include\webp\
-if errorlevel 1 exit 1
+cmake --install . --prefix %LIBRARY_PREFIX%
+if %ERRORLEVEL% neq 0 exit 1
